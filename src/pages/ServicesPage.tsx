bca59cad4ef575605d16f66ad70bae9d7bee38f0
Prantik Medhi <140103052+prantikmedhi@users.noreply.github.com>
2025-05-20 21:06:44 +0530

    Update ServicesPage.tsx

--- src/pages/ServicesPage.tsx
+++ src/pages/ServicesPage.tsx
@@ -8,7 +8,7 @@ import ScrollReveal from '../components/ui/ScrollReveal';
 const serviceImages = {
   emergency: '/IMG_7531.JPEG',
   opd: '/IMG_7211.JPG',
-  surgery: 'https://images.unsplash.com/photo-1579154204601-01588f351e67?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+  surgery: '/IMG_7537.JPG',
   maternity: 'https://images.unsplash.com/photo-1632053002424-602900664db1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
   criticalCare: 'https://images.unsplash.com/photo-1519494026892-80bbd2d6fd0d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
   diabetology: 'https://images.unsplash.com/photo-1576671414121-aa7c5830366b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
